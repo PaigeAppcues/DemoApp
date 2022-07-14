ad0ad7ea89bfa7d95f5e8feda1d96ea60d07a1c8
Paige Thowson <paigethowson@Paiges-MBP.lan>
2022-07-14 09:33:47 -0500

    added plants correction

--- src/Plants.js
+++ src/Plants.js
@@ -1,12 +1,12 @@
 import React from "react";
-import airpurifier from "./resources/plant.jpeg";
+import air_purifier from "./resources/indoor-air-purifying-plants.jpeg";
 
 const Plants = () => {
     return (
         <div className="app-content">
         <h1>Let's Check Out More Plants!</h1>
         <h2>Air Purifiers</h2>
-        <img src={airpurifier} alt="variety of air purifying plants"></img>
+        <img src={air_purifier} alt="variety of air purifying plants"></img>
       </div>
     );
   };
